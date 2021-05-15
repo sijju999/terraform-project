f49b8a06b4ef6f3ba6e11f122cc5f836a0defc47
Beat Studios <beatstudiz@gmail.com>
2021-05-15 23:43:51 +0530

    security

--- securitygcp.tf
+++ securitygcp.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "vprofile-bean-elb-sg" {
     protocol    = "tcp"
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
-
+  }
 }
 
 resource "aws_security_group" "vprofile-bastion-sg" {
@@ -31,7 +31,7 @@ resource "aws_security_group" "vprofile-bastion-sg" {
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.MYIP]
   }
 }
 
